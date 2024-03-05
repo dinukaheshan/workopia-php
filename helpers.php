45ddbb181d177da59041008baab757eb20fa7de7
dinukah99 <dinukahamarasinghe@gmail.com>
2024-03-05 17:51:23 +0530

    inspect helper functions

--- helpers.php
+++ helpers.php
@@ -29,7 +29,6 @@ function loadView($name) {
  * @param string $name
  * @return void    
  */
-
 function loadPartial($name) {
     $partialPath = basePath("views/partials/{$name}.php");
     if (file_exists($partialPath)) {
@@ -37,4 +36,27 @@ function loadPartial($name) {
     } else {
         echo "Partial '{$name}' not found";
     }
+}
+
+
+/**
+ * Inspect a value(s)
+ * @param mixed $value
+ * @return void    
+ */
+function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * @param mixed $value
+ * @return void    
+ */
+function inspectAndDie($value) {
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
 }
