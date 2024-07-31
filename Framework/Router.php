27c5e028a4c1c92921fbdba58eeb4c024f42fe2b
dinukah99 <dinukahamarasinghe@gmail.com>
2024-07-31 19:18:41 +0530

    refactor router for controller classes

--- Framework/Router.php
+++ Framework/Router.php
@@ -9,14 +9,18 @@ class Router {
      * Add a new route
      * @param string $method
      * @param string $uri
-     * @param string $controller
+     * @param string $action
      * @return void
      */
-    public function registerRoute($method, $uri, $controller) {
+    public function registerRoute($method, $uri, $action) {
+
+        list($controller,$controllerMethod) = explode('@', $action);
+
         $this->routes[] = [
             'method' => $method,
             'uri' => $uri,
-            'controller' => $controller
+            'controller' => $controller,
+            'controllerMethod' => $controllerMethod
         ];
     }
 
@@ -80,8 +84,15 @@ class Router {
     public function route($uri, $method) {
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === $method) {
-                require basePath('App/' . $route['controller']);
+                //Extract controller and controller method
+                $controller = 'App\\Controllers\\' . $route['controller'];
+                $controllerMethod = $route['controllerMethod'];
+
+                //Instantiate the controller and call the method
+                $controllerInstance = new $controller();
+                $controllerInstance -> $controllerMethod();
                 return;
+
             }
         }
         $this->error();
