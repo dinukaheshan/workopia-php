7f7ba51360fe9d75a40a47a21aa0078d5b6de6a5
dinukah99 <dinukahamarasinghe@gmail.com>
2024-07-31 19:28:48 +0530

    refactor all listings into a single ListingsController file

--- routes.php
+++ routes.php
@@ -2,3 +2,6 @@
 
 // All application routes
 $router->get('/', 'HomeController@index');
+$router->get('/listings', 'ListingController@index');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listing', 'ListingController@show');
