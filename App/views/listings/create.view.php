a5b112c01bbfa14f52ca97a1f43170b310496c88
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-07 23:11:56 +0530

    fix issue with the url routes for create listing

--- App/views/listings/create.view.php
+++ App/views/listings/create.view.php
@@ -14,6 +14,13 @@
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
             </h2>
+            <?php if (isset($errors)) : ?>
+                <?php foreach($errors as $error) : ?>
+                    <div class="message bg-red-100 my-3">
+                        <?= $error; ?>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
             <div class="mb-4">
                 <input type="text" name="title" placeholder="Job Title" class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
@@ -59,9 +66,6 @@
         </form>
     </div>
 </section>
-
-
-
 
 <?= loadPartial('bottom-banner'); ?>
 <?= loadPartial('footer'); ?>
