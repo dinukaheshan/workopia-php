54bbdb4339a1ddd1583f3e4ac0c64cc9528dd978
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-14 21:24:45 +0530

    refactor user listing owner function to separate function

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use Framework\Database;
 use Framework\Validation;
 use Framework\Session;
+use Framework\Authorization;
 
 class ListingController {
 
@@ -148,7 +149,7 @@ class ListingController {
         }
 
         // Authorization
-        if (Session::get('user')['id'] !== $listing->user_id) {
+        if (!Authorization::isOwner($listing->user_id)) {
             $_SESSION['error_message'] = 'You are not authorized to delete this listing';
             return redirect('/listings/' . $listing->$id);
         }
